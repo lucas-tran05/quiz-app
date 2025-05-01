192a42e1f1ec1ee94155f1d482f38d93c4f059ae
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-01 20:36:41 +0700

    fix console log

--- src/utils/sendResult.jsx
+++ src/utils/sendResult.jsx
@@ -1,8 +1,6 @@
 const KEY = import.meta.env.VITE_SECRET_KEY || 'b23dcat034';
 
 async function sendResultToSheet(result) {
-    console.log('Gửi kết quả:', result);
-    console.log('Secret key:', KEY);
     const formData = new FormData();
     formData.append('email', result.email);
     formData.append('fullname', result.fullname);
@@ -11,10 +9,6 @@ async function sendResultToSheet(result) {
     formData.append('totalQuestions', result.total);
     formData.append('score', result.correct / result.total * 10);
     formData.append('secret', KEY);
-
-    for (let [key, value] of formData.entries()) {
-        console.log(`${key}: ${value}`);
-    }
 
     fetch('https://script.google.com/macros/s/AKfycbzJ4qGLNT8kHvsuh_EqiYGLW2cWydOOPHyDkn5GWTU53mL68Hn6-VyIh5mexXp9EQ0p/exec', {
         method: 'POST',
