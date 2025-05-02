1f6e720677d95292b4454088d40ee84763f2b9ad
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-02 17:01:22 +0700

    fix name

--- src/config/subjects.jsx
+++ src/config/subjects.jsx
@@ -2,5 +2,5 @@ export const subjects = [
     { value: "int1336", label: "Mạng máy tính" },
     { value: "bas1122", label: "Tư tưởng Hồ Chí Minh"},
     { value: "bas1221", label: "Pháp luật đại cương"},
-    { value: "bas1122_1", label: "Tư tưởng Hồ Chí Minh (Chương 1)"}
+    { value: "bas1122_lms", label: "Tư tưởng Hồ Chí Minh (LMS)"}
 ];
