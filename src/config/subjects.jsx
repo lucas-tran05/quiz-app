d80243efb3572782b840e4e088859f085be29d89
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-02 17:00:13 +0700

    update bas1122_1

--- src/config/subjects.jsx
+++ src/config/subjects.jsx
@@ -2,5 +2,5 @@ export const subjects = [
     { value: "int1336", label: "Mạng máy tính" },
     { value: "bas1122", label: "Tư tưởng Hồ Chí Minh"},
     { value: "bas1221", label: "Pháp luật đại cương"},
-    { value: "cs1010", label: "Chủ nghĩa xã hội khoa học", disabled: true }
+    { value: "bas1122_1", label: "Tư tưởng Hồ Chí Minh (Chương 1)"}
 ];
