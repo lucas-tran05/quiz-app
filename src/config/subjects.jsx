be8b7a6da3dd7f0abb0b525546c9fd5ca5136fea
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-02 17:11:28 +0700

    update file

--- src/config/subjects.jsx
+++ src/config/subjects.jsx
@@ -4,4 +4,8 @@ export const subjects = [
     { value: "bas1221", label: "Pháp luật đại cương"},
     { value: "bas1122_lms", label: "Tư tưởng Hồ Chí Minh (LMS)"},
     { value: "bas1122_2", label: "Tư tưởng Hồ Chí Minh (Chương 2)" },
+    { value: "bas1122_3", label: "Tư tưởng Hồ Chí Minh (Chương 3)", disabled: true },
+    { value: "bas1122_4", label: "Tư tưởng Hồ Chí Minh (Chương 4)", disabled: true },
+    { value: "bas1122_5", label: "Tư tưởng Hồ Chí Minh (Chương 5)", disabled: true },
+    { value: "bas1122_6", label: "Tư tưởng Hồ Chí Minh (Chương 6)", disabled: true },
 ];
