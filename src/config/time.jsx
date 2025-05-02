64e7c043c3cf8605cf5c40161e615cc82dab8708
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-02 15:53:56 +0700

    fix time

--- src/config/time.jsx
+++ src/config/time.jsx
@@ -1,5 +1,4 @@
 export const timeOptions = [
-    { value: "1", label: "1 phút" },
     { value: "15", label: "15 phút" },
     { value: "30", label: "30 phút" },
     { value: "60", label: "60 phút" },
