5df579822c7c8fd24e7a4d9868dc3c50bc76437c
lucas-tran05 <trancuong13112005@gmail.com>
2025-05-05 14:53:03 +0700

    prevent copycopy

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import './index.css'
 import { Router } from 'preact-router'
+import { useEffect } from 'preact/hooks'
 import Home from './pages/Home'
 import Exam from './pages/Exam'
 import Result from './pages/Result'
@@ -7,6 +8,21 @@ import Config from './pages/Config'
 import UploadPage from './pages/Upload'
 
 export function App() {
+  useEffect(() => {
+    const handleCopy = (e) => e.preventDefault();
+    const handleCut = (e) => e.preventDefault();
+    const handleContextMenu = (e) => e.preventDefault();
+
+    document.addEventListener('copy', handleCopy);
+    document.addEventListener('cut', handleCut);
+    document.addEventListener('contextmenu', handleContextMenu);
+
+    return () => {
+      document.removeEventListener('copy', handleCopy);
+      document.removeEventListener('cut', handleCut);
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
   return (
     <>
       <Router>
